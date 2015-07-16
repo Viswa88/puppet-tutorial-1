78bab006d7003e2e2bf05361418c4d232e2b22c1
Antonio Valle - G2 <avalle@gedos.es>
2015-07-16 16:38:14 +0200

    Added relative directory to $CALAVERA_HOME

--- puppet.build.sh
+++ puppet.build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 NIC="eth0"
+if [ -z "$CALAVERA_HOME" ]; then
+   CALAVERA_HOME="/data/devops/Calavera"
+fi
 name="dnsmasq"
 export MY_IP=$(ifconfig $NIC | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 
@@ -7,10 +10,10 @@ echo -n "[agent]
 server=${HOSTNAME}.calavera.biz
 " > puppet.conf
 
-> /data/devops/Calavera/dnsmasq.hosts/puppet
+> ${CALAVERA_HOME}/dnsmasq.hosts/puppet
 pushd `dirname $0`
 export USER=root
-echo "-- Ceating the puppet testing machines"
+echo "-- Creating the puppet testing machines"
 for NODE in puppet1 puppet2 puppet3 
 do
     docker stop $NODE > /dev/null 2>&1
@@ -21,7 +24,7 @@ do
     vagrant up --no-provision ${NODE}
     echo "-- Adding \"${NODE}\" to dns"
     C_IP=`docker inspect --format='{{.NetworkSettings.IPAddress}}' ${NODE}`
-    echo "${C_IP} ${NODE} ${NODE}.calavera.biz" >> /data/devops/Calavera/dnsmasq.hosts/puppet
+    echo "${C_IP} ${NODE} ${NODE}.calavera.biz" >> ${CALAVERA_HOME}/dnsmasq.hosts/puppet
     docker kill -s HUP dnsmasq
     echo "-- Adding to puppet master"
     ssh root@${NODE} -o "StrictHostKeyChecking no" "puppet agent -t"
