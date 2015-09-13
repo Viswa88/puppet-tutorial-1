d5bb90e1294c5f1fc4d03387929eb264bda4c7c2
Marc Gracia <mgbuddy@gmail.com>
2015-09-13 11:35:57 +0200

    Minor changes on puppet_configure, added a puppetmaster install script

--- configure_master.sh
+++ configure_master.sh
@@ -1,8 +1,7 @@
 service apache2 stop
-rm -rf /etc/puppet/ssl
-cat /etc/apache2/sites-enabled/puppetmaster.conf|sed -e "s/SSLCertificateFile .*/SSLCertificateFile \/var\/lib\/puppet\/ssl\/certs\/puppet.pem/g" -e "s/SSLCertificateKeyFile .*/SSLCertificateKeyFile \/var\/lib\/puppet\/ssl\/private_keys\/puppet.pem/g" > /tmp/puppetmaster.conf
-mv /tmp/puppetmaster.conf /etc/apache2/sites-enabled/puppetmaster.conf
-cat /etc/puppet/puppet.conf | sed -e '/certname.*$/d' -e '/dns_alt_names.*$/d'|sed 's/\[master\]/\[master\]\ncertname = puppet\ndns_alt_names ='$HOSTNAME','$HOSTNAME'.calavera.biz,puppet\n/g' > /tmp/puppet.conf
+rm -rf /var/lib/puppet/ssl/*
+HSTNAME=`echo $HOSTNAME|tr '[:upper:]' '[:lower:]'`
+cat /etc/puppet/puppet.conf | sed -e '/certname.*$/d' -e '/dns_alt_names.*$/d'|sed 's/\[master\]/\[master\]\ncertname = '"$HSTNAME"'.calavera.biz\ndns_alt_names ='$HSTNAME','$HSTNAME'.calavera.biz,puppet\n/g' > /tmp/puppet.conf
 mv /tmp/puppet.conf /etc/puppet/puppet.conf
 echo "Press <CTRL>-C when finished generation"
 puppet master --verbose --no-daemonize 
---
+++ install_puppetmaster.sh
@@ -0,0 +1,3 @@
+apt-get remove --purge puppetmaster-passenger puppet-common apache2
+apt-get autoremove --purge 
+apt-get install puppetmaster-passenger
